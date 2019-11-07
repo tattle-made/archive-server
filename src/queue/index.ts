1f2d2a6aad8c097f3a19e0daca5bff46e6960749
denny <denny.george90@gmail.com>
2019-11-06 23:07:34 -0800

    (feat) Add js bundle for bull-board. Queue has UI now

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -1,12 +1,20 @@
 import {Queue as QueueType, Job} from 'bull';
 import * as Queue from 'bull';
 import { CreateStoryRequestModel } from '../routes/fact-checked-stories/CreateStoryRequestModel';
+// tslint:disable-next-line:no-var-requires
+const { setQueues } = require('bull-board');
+import {Promise} from 'bluebird';
+
 
 class QueueManager {
     private factCheckStoriesIndexQueue: QueueType;
 
     constructor() {
         this.factCheckStoriesIndexQueue = new Queue('test');
+
+        setQueues([
+            this.factCheckStoriesIndexQueue,
+        ]);
     }
 
     public addFactCheckStoryIndexJob(jobParam: CreateStoryRequestModel) {
@@ -16,7 +24,8 @@ class QueueManager {
     public setupWorker() {
         this.factCheckStoriesIndexQueue.process((job) => {
             return Promise.resolve(job.toJSON())
-            .then((result) => console.log(result));
+            .then((result) => console.log(result))
+            .then(() => Promise.delay(5000));
         });
     }
 }
